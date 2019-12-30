c2adf83d4a5e273bdbbd67eff127b6a35b3ff6f4
lepea <darotudeen@gmail.com>
2019-12-30 13:43:34 +0100

    add menu to blogactivities

--- app/src/main/java/com/example/itexblog/ui/BlogActivitiesFragment.kt
+++ app/src/main/java/com/example/itexblog/ui/BlogActivitiesFragment.kt
@@ -4,10 +4,8 @@ package com.example.itexblog.ui
 import android.app.Application
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -87,5 +85,25 @@ class BlogActivitiesFragment : Fragment() {
 
         })
     }
+
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        super.onCreateOptionsMenu(menu, inflater)
+//        inflater.inflate(R.menu.post_menu, menu)
+//
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        val id = item.itemId
+//        when(id){
+//            R.id.action_settings -> {
+//                return true
+//            }
+//            R.id.action_info -> {
+//                Toast.makeText(context, "You clicked me", Toast.LENGTH_SHORT).show()
+//                return true
+//            }
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
 
 }
