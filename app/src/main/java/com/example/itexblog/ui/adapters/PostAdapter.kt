d76c77fd73787a2c3bebf92cabd14a5804e45229
lepea <darotudeen@gmail.com>
2019-12-31 14:52:47 +0100

    add delete functionality to delete button

--- app/src/main/java/com/example/itexblog/ui/adapters/PostAdapter.kt
+++ app/src/main/java/com/example/itexblog/ui/adapters/PostAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.itexblog.ui.adapters
 
+import android.app.Application
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
@@ -14,8 +15,13 @@ import androidx.databinding.adapters.AdapterViewBindingAdapter
 import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.RecyclerView
 import com.example.itexblog.R
+import com.example.itexblog.ui.model.PostDatabase
 import com.example.itexblog.ui.model.PostEntity
+import com.example.itexblog.ui.viewmodel.PostViewModel
 import com.squareup.picasso.Picasso
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 import org.w3c.dom.Text
 import androidx.navigation.Navigation as Navigation1
 
@@ -75,11 +81,12 @@ class PostAdapter(private var posts:List<PostEntity?>?, private var listener:OnP
         var num_of_likes = itemView.findViewById<TextView>(R.id.num_of_likes)
         var post_date =itemView.findViewById<TextView>(R.id.post_date)
         var divider = itemView.findViewById<View>(R.id.divider)
-        var card = itemView.findViewById<CardView>(R.id.card_container)
+        var deleteBtn = itemView.findViewById<View>(R.id.delete_btn)
 
 
 
         fun bind(postEntity: PostEntity?, listener: OnPostListener){
+            val postViewModel: PostViewModel?= null
             title.setText(postEntity?.title)
             body.setText(postEntity?.body)
             post_date.setText(postEntity?.date)
@@ -94,6 +101,16 @@ class PostAdapter(private var posts:List<PostEntity?>?, private var listener:OnP
                 divider.visibility = View.VISIBLE
             }
 
+            deleteBtn.setOnClickListener {view ->
+
+                postEntity?.let{
+                    CoroutineScope(IO).launch {
+                        PostDatabase.getInstance(view.context)?.postDao()?.delete(it)
+                    }
+                    Toast.makeText(view.context, "${postEntity.title} deleted", Toast.LENGTH_SHORT).show()
+                }
+
+            }
 
             itemView.setOnClickListener {
                 listener.onPostClick(postEntity)
